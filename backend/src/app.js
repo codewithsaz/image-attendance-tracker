2a3bc4e64e049f0a30bcfb80d5eb71a5939cc1ba
codewithsaz <codewithsaz@gmail.com>
2024-06-15 09:49:52 +0530

    serving client through server

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 
 //Database
 const db = require("./utils/database");
@@ -22,16 +23,24 @@ const LeaveRouter = require("./routes/leaveRoutes");
 //updateEmployeeStatus
 const updateShiftJob = require("./services/updateEmployeeShiftStatus");
 
+//serving client
+const _dirname = path.dirname("");
+console.log(_dirname);
+const buildpath = path.join(_dirname, "../client/dist");
+console.log(buildpath);
+app.use(express.static(buildpath));
+
+app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4000"],
     allowedHeader: true,
     credentials: true,
   })
 );
+
 app.use(helmet());
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
-app.use(express.json());
 
 app.use("/test", (req, res) => {
   res.status(200).json({ success: "true", message: "endpoints are working" });
@@ -40,6 +49,7 @@ app.use("/api/v1", EmployeeRouter);
 app.use("/api/v1", AdminRouter);
 app.use("/api/v1", AttendanceRouter);
 app.use("/api/v1", LeaveRouter);
+app.get("*", (req, res) => res.sendFile(path.resolve(buildpath, "index.html")));
 
 app.listen(process.env.PORT, () => {
   console.log("Server Started at ", process.env.PORT);
